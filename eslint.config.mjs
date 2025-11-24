0345110a69abe0462eea58b37946ce4ac2e93e90
Alex Hedges <git@alexhedges.dev>
2025-11-24 18:28:47 -0500

    simplify eslint config
    https://typescript-eslint.io/getting-started/
    https://typescript-eslint.io/packages/typescript-eslint/
    
    npm install --save-dev eslint @eslint/js typescript typescript-eslint
    npm uninstall @eslint/eslintrc @typescript-eslint/parser @typescript-eslint/eslint-plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,23 +2,16 @@ import { defineConfig, globalIgnores } from "eslint/config";
 import typescriptEslint from "@typescript-eslint/eslint-plugin";
 import globals from "globals";
 import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-	baseDirectory: __dirname,
-	recommendedConfig: js.configs.recommended,
-	allConfig: js.configs.all
-});
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
 
 export default defineConfig([
 	globalIgnores(["**/dist"]),
 	{
-		extends: compat.extends("eslint:recommended", "plugin:@typescript-eslint/recommended"),
+		extends: [
+			eslint.configs.recommended,
+			tseslint.configs.recommended,
+		],
 
 		plugins: {
 			"@typescript-eslint": typescriptEslint,
