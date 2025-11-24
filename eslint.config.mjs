8df63cfd3473d9aec062b99fef992d168c8fc3a1
Alex Hedges <git@alexhedges.dev>
2025-11-24 15:58:00 -0500

    migrate es formats
    ```
    npx @eslint/migrate-config@2.0.0 .eslintrc.json
    rm .eslintrc.json
    npm install globals @eslint/js @eslint/eslintrc --save-dev
    eslint eslint.config.mjs --fix
    ```

---
+++ eslint.config.mjs
@@ -0,0 +1,43 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+	baseDirectory: __dirname,
+	recommendedConfig: js.configs.recommended,
+	allConfig: js.configs.all
+});
+
+export default defineConfig([globalIgnores(["**/dist"]), {
+	extends: compat.extends("eslint:recommended", "plugin:@typescript-eslint/recommended"),
+
+	plugins: {
+		"@typescript-eslint": typescriptEslint,
+	},
+
+	languageOptions: {
+		globals: {
+			...globals.browser,
+			...globals.webextensions,
+		},
+
+		parser: tsParser,
+		ecmaVersion: 11,
+		sourceType: "module",
+	},
+
+	rules: {
+		"@typescript-eslint/no-non-null-assertion": 0,
+		indent: ["error", "tab"],
+		"linebreak-style": ["error", "unix"],
+		quotes: ["error", "double"],
+		semi: ["error", "always"],
+	},
+}]);
